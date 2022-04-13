3f6ea02fa590314eb97ee56949182c48696b6f83
aysutoprak <moonwater99@hotmail.com>
2022-04-13 16:08:35 +0300

    updates

---
+++ facerecognitionapi/controllers/image.js
@@ -0,0 +1,33 @@
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+    apiKey: "a6195df3c9f549509de4d259f6af7ad8",
+  });
+  
+const handleApiCall = (req, res) => {
+     app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input) 
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+  }
+  
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db("users")
+      .where("id", "=", id)
+      .increment("entries", 1)
+      .returning("entries")
+      .then((entries) => {
+        console.log(entries);
+        res.json(entries[0].entries);
+      })
+      .catch((err) => res.status(400).json("Unable to get entry count"));
+  };
+
+  module.exports = {
+      handleImage,
+      handleApiCall
+  }
